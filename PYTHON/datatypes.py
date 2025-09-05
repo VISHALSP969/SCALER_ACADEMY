7efd2c3e49fc746da2d26925aaf6ac2fbd1869ca
VISHALSP969 <vishalsp969@gmail.com>
2025-09-06 00:14:09 +0530

    Python Basic Datatypes

---
+++ PYTHON/datatypes.py
@@ -0,0 +1,58 @@
+# Integer datatype
+number=24
+print(number)
+print(type(number))
+
+a=50
+print(a)
+print(type(a))
+
+print("a")
+
+# Float datatype
+f=1.0
+print(f)
+print(type(f))
+
+pi=3.14
+print(pi)
+print(type(pi))
+
+# Boolean datatype
+isRaining=True
+print(isRaining)
+print(type(isRaining))
+
+isCloudy=False
+print(isCloudy)
+print(type(isCloudy))
+
+# String datatype
+str1='String1'
+print(str1)
+print(type(str1))
+str2="String2"
+print(str2)
+print(type(str2))
+str3='''String3'''
+print(str3)
+print(type(str3))
+str4="""String4"""
+print(str4)
+print(type(str4))
+
+str5="""multi
+line
+string"""
+print(str5)
+print(type(str5))
+str6='''another
+multi line
+string'''
+print(str6)
+print(type(str6))
+
+# None datatype
+n=None
+print(n)
+print(type(n))
