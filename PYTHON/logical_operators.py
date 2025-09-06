e5b08da338b339a0e3a3d312784305ebc21b2ee9
VISHALSP969 <vishalsp969@gmail.com>
2025-09-07 02:28:59 +0530

    Logical Operators

---
+++ PYTHON/logical_operators.py
@@ -0,0 +1,24 @@
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+
+print(5>3 and 5>2)
+print(2>3 and 3>2)
+print(5>3 and 2>5)
+print(2>5 and 3>5)
+
+print(True or True)
+print(False or True)
+print(True or False)
+print(False or False)
+
+print(5>3 or 5>2)
+print(2>3 or 3>2)
+print(5>3 or 2>5)
+print(2>5 or 3>5)
+
+print(not True)
+print(not False)
+print(not 2<5)
+print(not 2>5)
