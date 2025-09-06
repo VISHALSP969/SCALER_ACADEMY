83345926bba36374b640a25c0bb480dafabfb0b5
VISHALSP969 <vishalsp969@gmail.com>
2025-09-06 18:48:57 +0530

    Comparison Operators

---
+++ PYTHON/comparison_operators.py
@@ -0,0 +1,25 @@
+a=6
+b=4
+print(a==b)
+print(3==3)
+
+print(a!=b)
+print(3!=3)
+
+print(5>3)
+print(3>5)
+
+print(5<3)
+print(3<5)
+
+print(5>=3)
+print(3>=5)
+
+print(5<=3)
+print(3<=5)
+
+# Strings
+print("Hello"=="Hello")
+print("Hello"=="hello")
+print("Hello">"hello")
+print("hello">"Hello")
