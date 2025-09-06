68571bf80a2cdeb17172aaf3783f41d1027aa2f8
VISHALSP969 <vishalsp969@gmail.com>
2025-09-06 18:28:28 +0530

    Arithmetic Operators

---
+++ PYTHON/arithmetic_operators.py
@@ -0,0 +1,28 @@
+a=5
+b=3
+
+print(a+b)
+print(a-b)
+print(b-a)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+print(1.5+1)
+print(5-1.3)
+print(1.3*3)
+print(5.6/0.8)
+print(0.2**2)
+print(5.5//0.9)
+print(5.5%0.9)
+# String concatenation
+print("Hello "+"World")
+first="John"
+last="Doe"
+print(first+" "+last)
+print("1"+"1")
+# print("Hello"+1)    # Type Error
+# print(1+"Hello")    #Type Error
+print("Hello"*3)
