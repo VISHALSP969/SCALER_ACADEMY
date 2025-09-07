7e28000525dff6e2abd53a19bd5fa60002e3ce45
VISHALSP969 <vishalsp969@gmail.com>
2025-09-07 21:00:08 +0530

    Membership Operator & Identity Operator

---
+++ PYTHON/special_operators.py
@@ -0,0 +1,21 @@
+str1="Sample String"
+print('S' in str1)
+print('s' in str1)
+print("Sample" in str1)
+print('string' in str1)
+
+print('S' not in str1)
+print('s' not in str1)
+print("Sample" not in str1)
+print('string' not in str1)
+
+x=5
+y=5
+z=10
+print(x is y)
+print(x is z)
+print(y is z)
+
+print(x is not y)
+print(x is not z)
+print(y is not z)
