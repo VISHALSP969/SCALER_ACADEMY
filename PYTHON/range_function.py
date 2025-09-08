c284047dd0378618d6a609b65be27e68b59c8904
VISHALSP969 <vishalsp969@gmail.com>
2025-09-09 02:32:54 +0530

    range() function

---
+++ PYTHON/range_function.py
@@ -0,0 +1,17 @@
+print(range(5))
+print(list(range(5)))
+
+print(range(2,5))
+print(list(range(2,5)))
+
+print(range(1,10,2))
+print(list(range(1,10,2)))
+
+print(range(1,10,3))
+print(list(range(1,10,3)))
+
+print(range(10,1,-1))
+print(list(range(10,1,-1)))
+
+print(range(-1,-10,-1))
+print(list(range(-1,-10,-1)))
