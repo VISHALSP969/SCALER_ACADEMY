0ce444da38f6820d1f645376681f709f97f74741
VISHALSP969 <vishalsp969@gmail.com>
2025-09-09 21:19:39 +0530

    Break,Contunue and Pass Statement

---
+++ PYTHON/break_continue_pass.py
@@ -0,0 +1,18 @@
+for i in range(1,5):
+    if i==3:
+        break
+    print(i)
+print()
+
+for j in range(1,5):
+    if j==3:
+        continue
+    print(j)
+print()
+
+for k in range(1,10):
+    pass
+
+def func():
+    pass
+func()
