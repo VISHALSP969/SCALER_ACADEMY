b9c992e48885786b146f51101092edd86d3dc249
VISHALSP969 <vishalsp969@gmail.com>
2025-09-10 10:55:03 +0530

    Python Strings

---
+++ PYTHON/strings.py
@@ -0,0 +1,27 @@
+s="Sample String"
+print(s)
+print(type(s))
+
+s1="Sample String"
+print(s1)
+print(type(s))
+
+s2='''Sample String'''
+print(s2)
+print(type(s2))
+
+s3="""Sample String"""
+print(s3)
+print(type(s3))
+
+s4='''Multi
+Line
+String'''
+print(s4)
+print(type(s4))
+
+s5="""Multi
+Line
+String"""
+print(s5)
+print(type(s5))
