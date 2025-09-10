9bfec653b580e6b063981aaec9dd75293955d7e1
VISHALSP969 <vishalsp969@gmail.com>
2025-09-10 20:53:30 +0530

    String Formatting

---
+++ PYTHON/string_formatting.py
@@ -0,0 +1,6 @@
+name="John"
+age=25
+
+print("Hey,my name is",name,"And my age is",age)
+print("Hey,my name is {}.And my age is {}.".format(name,age))
+print(f"Hey,my name is {name}.And my age is {age}.")
