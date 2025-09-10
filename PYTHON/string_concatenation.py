82e4def144f4b4d9eda12d273cbe10a33894e94e
VISHALSP969 <vishalsp969@gmail.com>
2025-09-10 21:10:06 +0530

    String Concatenation

---
+++ PYTHON/string_concatenation.py
@@ -0,0 +1,14 @@
+first="John"
+last="Doe"
+print(first+last)
+print(first+" "+last)
+
+print("a"+"a")
+print("a"+"a"+"a")
+print(2+2+2)
+print("2"+"2"+"2")
+
+# print(2+"2")      #Type Error
+
+print(2*3)
+print("2"*3)
