cbdec323b61ee5a86bc6d3404b806c20fce5a040
VISHALSP969 <vishalsp969@gmail.com>
2025-09-11 21:14:53 +0530

    List Slicing

---
+++ PYTHON/list2.py
@@ -0,0 +1,17 @@
+l=[1,2,3,4,5,6,7,8,9]
+print(l)
+print(len(l))
+print(l[::])
+print(l[::2])
+print(l[::3])
+print(l[:5])
+print(l[2:])
+print(l[2:5])
+print(l[2:7])
+print(l[2:7:2])
+print(l[2:7:3])
+
+print(l[::-1])
+print(l[-5:-1])
+print(l[-1:-6:-1])
+print(l[-6:-1:1])
