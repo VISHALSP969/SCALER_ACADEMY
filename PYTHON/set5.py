92cea0bd2853e71d486ec88390e21b0c4fa45a30
VISHALSP969 <vishalsp969@gmail.com>
2025-09-15 02:18:13 +0530

    Difference of sets

---
+++ PYTHON/set5.py
@@ -0,0 +1,6 @@
+python={"Iron man","Hulk","Spidy","Rahu"}
+java={"Iron man","Harry potter","Ant man"}
+print(python.difference(java))
+print(java.difference(python))
+print(python-java)
+print(java-python)
