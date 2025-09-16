1a3d255fa991e3c4beff0f14ac27ac83657ee5a5
VISHALSP969 <vishalsp969@gmail.com>
2025-09-16 19:51:23 +0530

    Return statement

---
+++ PYTHON/return.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    c=a+b
+    return c
+
+sum=add(2,3)
+print(sum)
+
+def func():
+    print("Demo function")
+c=func()
+print(c)
+
+def func1():
+    print("Before return")
+    return "Nothing"
+print(func1())
