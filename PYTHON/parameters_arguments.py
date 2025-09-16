89aa380b5821e452d797e8a965d132dca9c2ac1a
VISHALSP969 <vishalsp969@gmail.com>
2025-09-16 16:59:26 +0530

    Arguments & Parameters

---
+++ PYTHON/parameters_arguments.py
@@ -0,0 +1,15 @@
+# function definition
+def greet(name):
+    """
+    This function greet everyone when it is called
+    """
+    print("Hey, how are you !!",name)
+
+# function call
+greet("John")
+
+def add(a,b):
+    c=a+b
+    print(c)
+
+add(2,3)
