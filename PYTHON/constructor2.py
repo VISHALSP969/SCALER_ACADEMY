4c5312b50c1afebba52a7d1da207ae4e265981d1
VISHALSP969 <vishalsp969@gmail.com>
2025-09-19 10:44:59 +0530

    Attribute initialization using constructor

---
+++ PYTHON/constructor2.py
@@ -0,0 +1,16 @@
+class Human:
+    def __init__(self,name,age,hobby):
+        self.name=name
+        self.age=age
+        self.hobby=hobby
+
+obj1=Human("John",25,"Reading")
+obj2=Human("Emma",23,"Cooking")
+print(obj1)
+print(obj1.name)
+print(obj1.age)
+print(obj1.hobby)
+print(obj2)
+print(obj2.name)
+print(obj2.age)
+print(obj2.hobby)
