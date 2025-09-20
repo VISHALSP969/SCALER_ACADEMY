3b3d28072035938ef48538824fb8e66fded9584e
VISHALSP969 <vishalsp969@gmail.com>
2025-09-21 01:19:13 +0530

    Errors in Python

---
+++ PYTHON/errors.py
@@ -0,0 +1,16 @@
+# if x>0        # syntax error
+#     print("x is positive")
+
+# print(a)      # NameError
+
+# print(2+"2")      # TypeError
+
+# with open("NotFound.txt","r") as f:       #FileNotFoundError
+#     data=f.read()
+
+# if True:
+# print("Hello")        # IndentationError
+
+# print(5/0)        #ZeroDivisionError
+
+print(dir(__builtins__))
